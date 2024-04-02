c13ef06369985dbae2d8d4a5a85d0bbc1a992364
Andyliu <andyliu.pub@outlook.com>
2024-04-02 08:07:56 +0000

    added gdb to dev container

--- CuCAMASim/util/config.cpp
+++ CuCAMASim/util/config.cpp
@@ -1,9 +1,10 @@
 #include "config.h"
 
-
+#include <yaml-cpp/yaml.h>
 
 #include <iostream>
 
 camConfig::camConfig(std::string configPath) {
   std::cout << "Using config: " << configPath << std::endl;
+  YAML::Node cam_config = YAML::LoadFile(configPath);
 }
--- main.cpp
+++ main.cpp
@@ -7,10 +7,7 @@ using namespace std;
 
 int main() {
   cout << "hello world!" << endl;
-
-  YAML::Node config = YAML::LoadFile("/workspaces/CuCAMASim/accuracy_with_hardboundary.yml");
-
-  camConfig cam_config("accuracy_with_hardboundary.yml");
+  camConfig cam_config("/workspaces/CuCAMASim/accuracy_with_hardboundary.yml");
   CuCAMASim camasim;
   return 0;
 }
