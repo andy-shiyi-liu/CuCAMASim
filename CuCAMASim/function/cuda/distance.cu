b9f3463509e5c090fa66537041033a6f2b41101b
Andyliu <andyliu.pub@outlook.com>
2024-04-04 07:29:16 +0000

    Refactor CMakeLists.txt and FunctionSimulator.cpp

---
+++ CuCAMASim/function/cuda/distance.cu
@@ -0,0 +1,6 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
+
+#include <cuda_runtime.h>
+
+#endif
--- CuCAMASim/include/function/search.h
+++ CuCAMASim/include/function/search.h
@@ -1,6 +1,20 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
-class CAMSearch{};
+#include <iostream>
+#include <string>
+
+#include "config.h"
+
+class CAMSearch {
+//  private:
+//   QueryConfig *queryConfig;
+//   ArrayConfig *arrayConfig;
+//   const std::string searchScheme;
+//   const double searchParameter;
+//   const std::string sensing;
+//   const double sensingLimit;
+  
+};
 
 #endif  // SEARCH_H
